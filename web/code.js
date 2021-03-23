00d108bebcb2e2ef1277c7ec6374577dcf651f44
TheBotlyNoob <72417807+TheBotlyNoob@users.noreply.github.com>
2021-03-23 11:41:13 -0400

    PUSH yay

--- web/code.js
+++ web/code.js
@@ -1,17 +1,17 @@
 "use strict";
 
-function getFile(url) {
+const getFile = async url => {
   var contents;
-  contents = $.get(url)
+  contents = $.get(url);
   return contents;
-}
+};
 
-function filterLines(url, filter) {
+const filterLines = async (url, filter) => {
   var linecontents = [],
       lines;
-  lines = getFile(url);
-  lines = lines.toString().split("\n")
-  console.log(lines)
+  lines = await getFile(url);
+  lines = lines.toString().split("\n");
+  console.log(lines);
 
   for (var i = 0; i < lines.length; i++) {
     if (lines[i].includes(filter)) {
@@ -20,16 +20,17 @@ function filterLines(url, filter) {
   }
 
   return linecontents;
-}
+};
 
+(async () => {
+  var lines = await filterLines("/tunnel.log", "your url is:");
 
-var lines = filterLines("/tunnel.log", "your url is:")
-
-if (lines.length != 0) {
+  if (lines.length != 0) {
     lines = lines[lines.length - 1].match(new RegExp("https://[A-Za-z0-9./\-]*")).toString();
     $("#tunnelurl").html(`Your LocalTunnel URL Is: ${lines} On Port 22023`);
     lines = lines.replace('https://', '');
     $("#serverinfo").attr('href', `https://thebotlynoob.github.io/Among-Us-Heroku/#${lines}:22023`).html("Click To Get The Server File!");
-} else {
+  } else {
     $("#tunnelurl").html(`You Don't Have A LocalTunnel URL... Try Again`);
-}
+  }
+})();
