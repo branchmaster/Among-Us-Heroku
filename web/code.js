fc2a2e8217a1cc139a7c88e11e78dc1728d2f00f
TheBotlyNoob <thebotlynoob@gmail.com>
2021-03-24 13:37:45 -0400

    web interface should work

--- web/code.js
+++ web/code.js
@@ -1,6 +1,6 @@
 "use strict";
 
-(async () => {
+const main = async () => {
     var lines;
     await $.ajax({
         url: "/tunnel.log",
@@ -19,19 +19,20 @@
 
     if (lines !== '') {
         lines = lines.match(new RegExp("https://[A-Za-z0-9./\-]*")).toString();
-        $("#tunnelurl").html(`Your LocalTunnel URL Is: ${lines} On Port 22023`);
-        $("<hr>").appendTo("body");
-        $("<div/>", {
-            id: 'serverinfoContainer'
-        }).appendTo("body");
+        $("#tunnelurl").html(`Your LocalTunnel URL Is: ${lines}`);
         lines = lines.replace('https://', '');
+        $("<br/>",{}).appendTo("#tunnel");
         $("<a/>", {
             id: 'serverinfo',
-            href: `https://thebotlynoob.github.io/Among-Us-Heroku/#${lines}:22023`,
+            href: `https://impostor.github.io/Impostor/#${lines}`,
             text: 'Click Here To Get The Server File!',
             target: '_blank'
-        }).appendTo("#serverinfoContainer");
+        }).appendTo("#tunnel");
     } else {
         $("#tunnelurl").html(`You Don't Have A LocalTunnel URL... Try Again`);
     }
-})();
+};
+
+if (typeof require !== 'undefined' && require.main === module) {
+    main();
+}
