38e302d95d20dc39b36dc6b3c6b20a1e9c6da010
TheBotlyNoob <thebotlynoob@gmail.com>
2021-03-26 00:20:23 -0400

    php again

---
+++ web/code.php
@@ -0,0 +1,10 @@
+<?php $url = file_get_contents("tunnel.log");
+$url = preg_match('/tcp:\/\/(.+:[0-9]+) /', $url, $matches) ? $matches[1] : '';
+if ($url !== ''){
+    echo ("Your URL Is: " . $url);
+}
+else
+{
+    echo ("You Don't Have A URL... Try Again");
+}
+?>
--- web/index.php
+++ web/index.php
@@ -13,15 +13,26 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
-  <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
+  <!-- If I decide I want to go back to JS <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script> -->
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <noscript>JavaScript Is Required</noscript>
 <div id="tunnel">
-<h3 id="tunnelurl"></h>
+<h3 id="tunnelurl"><?php include("code.php");?></h>
 </div>
-<div id="serverinfocontainer"></div>
-<script src="code.js"></script>
+<hr>
+<div id="serverinfocontainer">
+    <a id="serverinfo" href="<?php if ($url !== '') {
+        echo("https://impostor.github.io/Impostor/#" . $url);
+        }
+        ?>" target="_blank">
+        <?php if ($url !== '') {
+            echo("Click Here To Get The Server File!");
+        }
+        ?>
+    </a>
+</div>
+<!-- If I decide I want to go back to JS <script src="code.js"></script> -->
 </body>
 </html>
