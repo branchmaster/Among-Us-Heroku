9af8fae47ea3a57f2ae1be6d0a90291c922fa371
Jay Jackson <thebotlynoob@gmail.com>
2021-03-31 17:11:11 +0000

    added a few things to the web interface

--- web/code.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$url = file_get_contents(__DIR__."/tunnel.log");
-if ($url !== "") {
-    echo ("Your URL Is: $url");
-}
-else {
-    echo ("You Don't Have A URL... Try Again");
-}
-?>
--- web/index.php
+++ web/index.php
@@ -9,31 +9,42 @@
     <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
     <link rel="icon" type="image/x-icon" href="favicons/favicon.ico">
-  
-  <!-- If I decide I want to go back to JS <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script> -->
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<!-- If I decide I want to go back to JS <noscript>JavaScript Is Required</noscript> -->
 <div id="tunnel">
-<h3 id="tunnelurl"><?php require(__DIR__."/code.php");?></h>
+<h3 id="tunnelurl"><?php 
+try {
+    $url = @file_get_contents(__DIR__."/tunnel.log");
+} catch(Exception $e) {
+    echo("Error $e->getMessage()");
+    $url = false;
+}
+if ($url !== false) {
+    echo ("Your URL Is: $url");
+}
+else {
+    echo ("The URL File Doesn´t Seem To Exist... Try Deploying Again, If It Still Doesn´t Work Then Make An Issue At <a id='issuepage' target='_blank' rel='noopener noreferrer' href='https://github.com/TheBotlyNoob/Among-Us-Heroku/issues/new'>The GitHub Page</a>");
+}
+?>
+</h>
 </div>
 <hr>
 <div id="serverinfocontainer">
-    <a style="<?php if ($url === "") {
+    <a style="<?php if ($url === false) {
         echo("display: none;");
     } 
-    ?>" id="serverinfo" href="<?php if ($url !== "") {
-            $url = str_replace("tcp://", "", $url);
+    ?>" id="serverinfo" href="<?php if ($url !== false) {
+            $url = str_replace("tcp://", false, $url);
             echo("https://impostor.github.io/Impostor/#$url");
         }
         ?>" target="_blank" rel="noopener noreferrer">
-        <?php if ($url !== "") {
+        <?php if ($url !== false) {
             echo("Click Here To Get The Server File!");
         }
         ?>
     </a>
 </div>
-<!-- If I decide I want to go back to JS <script src="code.js"></script> -->
+<a rel="noopener noreferrer" class="github" href="https://github.com/TheBotlyNoob/Among-Us-Heroku" target="_blank"><img src="https://img.icons8.com/nolan/128/github.png" alt="github"/>Visit The GitHub Page!</a>
 </body>
 </html>
