affdb06ad6e794263a1ed260f3409785966987ec
Jay Jackson <>
2021-04-03 23:20:15 +0000

    better error page

--- web/index.php
+++ web/index.php
@@ -20,10 +20,10 @@
         echo ("Your URL Is: <a target='_blank' rel='noopener noreferrer' href='{$url}'>{$url}</a>");
     }
     else {
-        echo ("The URL File Doesn't Seem To Exist...\nTry Deploying Again, If It Still Doesn't Work Then Make An Issue At <a target='_blank' rel='noopener noreferrer' href='https://github.com/TheBotlyNoob/Among-Us-Heroku/issues/new'>The GitHub Page</a>");
+        echo ("The URL File Doesn't Seem To Exist,<br>Try Deploying Again, If It Still Doesn't Work Then Make An Issue At <a target='_blank' rel='noopener noreferrer' href='https://github.com/TheBotlyNoob/Among-Us-Heroku/issues/new'>The GitHub Page</a>");
     }
 } catch(Exception $e) {
-    echo("{$e->getMessage()}\nTry Deploying Again, If It Still Doesn't Work Then Make An Issue At <a target='_blank' rel='noopener noreferrer' href='https://github.com/TheBotlyNoob/Among-Us-Heroku/issues/new'>The GitHub Page</a>");
+    echo("{$e->getMessage()},<br>Try Deploying Again, If It Still Doesn't Work Then Make An Issue At <a target='_blank' rel='noopener noreferrer' href='https://github.com/TheBotlyNoob/Among-Us-Heroku/issues/new'>The GitHub Page</a>");
 }
 ?>
 </h>
